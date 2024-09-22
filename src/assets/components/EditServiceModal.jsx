82a5f98a4409e006e7a239ea2b0fd17cb0a8fbf2
Sanjay Bhandari <sanjay.bhandari1909@gmail.com>
2024-09-22 11:33:53 +0530

    Add edit service functionality

--- src/assets/components/EditServiceModal.jsx
+++ src/assets/components/EditServiceModal.jsx
@@ -1,9 +1,24 @@
 import React, { useState } from 'react'
 
-const EditServiceModal = ({ isEditServiceModalOpen, closeEditServiceModal,index,service }) => {
+const EditServiceModal = ({ isEditServiceModalOpen, closeEditServiceModal,service,onEditService }) => {
 
   if (!isEditServiceModalOpen) return null;
   const [formData,setFormData]=useState(service);
+  
+  const onEdit=()=>{
+    onEditService(service.index,formData)
+    setFormData({ name: "", price: 0, description: "" });
+    closeEditServiceModal();
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: name === "price" ? parseFloat(value) : value, // Convert price to a float
+    }));
+  };
+
 return (
   <div  className="fixed inset-0 z-50 flex justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
   <div className="relative p-4 w-full max-w-md max-h-full">
@@ -26,21 +41,21 @@ return (
               <div className="grid gap-4 mb-4 grid-cols-2">
                   <div className="col-span-2">
                       <label htmlFor="name" className="block mb-2 text-sm font-medium text-blue-500 ">Name</label>
-                      <input value={formData.name} type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-blue-500 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"/>
+                      <input value={formData.name} onChange={handleChange} type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-blue-500 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"/>
                   </div>
                   <div className="col-span-2 sm:col-span-1">
                       <label htmlFor="price" className="block mb-2 text-sm font-medium text-blue-500 dark:text-blue-500">Price</label>
-                      <input value={formData.price} type="number" name="price" id="price" className="bg-gray-50 border border-gray-300 text-blue-500 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"/>
+                      <input value={formData.price} onChange={handleChange} type="number" name="price" id="price" className="bg-gray-50 border border-gray-300 text-blue-500 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"/>
                   </div>
                   <div className="col-span-2 sm:col-span-1">
                     
                   </div>
                   <div className="col-span-2">
                       <label htmlFor="description" className="block mb-2 text-sm font-medium text-blue-500 dark:text-blue-500">Service Description</label>
-                      <textarea value={formData.description} id="description" rows="4" className="block p-2.5 w-full text-sm text-blue-500 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"></textarea>                    
+                      <textarea value={formData.description} onChange={handleChange} name='description' id="description" rows="4" className="block p-2.5 w-full text-sm text-blue-500 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"></textarea>                    
                   </div>
               </div>
-              <button type="button" className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">
+              <button onClick={onEdit} type="button" className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">
                   Edit this service
               </button>
           </form>
